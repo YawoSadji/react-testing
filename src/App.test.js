476b7b4e50e5633a326eb21efadb9711565a56ea
YawoSadji <yawo.sadji@gmx.com>
2025-02-25 15:11:18 -0500

    added some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getByText, waitFor } from '@testing-library/react';
 import { App } from './App';
 
 /**
@@ -13,26 +13,37 @@ test('App should render', () => {
   expect(screen.getByText('Welcome, party people!')).toBeInTheDocument();
 });
 
-test('Button should render', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+test('it renders the theme toggle button', () => {
+  const {getByText} = render(<App />);
+  expect(getByText('Current theme: light')).toBeInTheDocument();
 });
 
 /**
  * Verify clicking button should change theme
  * hint: use fireEvent.click(element) to trigger a click event on an element
  */
-test('theme button should update button text', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+test('theme button should update button text', async() => {
+  const {getByText} = render(<App />);
+  const themeButtonToggle = getByText('Current theme: light');
+  expect(getByText('Current theme: light')).toBeInTheDocument();
+  fireEvent.click(themeButtonToggle);
+  await waitFor(()=>expect(getByText('Current theme: dark')).toBeInTheDocument());
 });
 
 // BONUS
 // hint: there is a `.toHaveStyle` method.
 // e.g.: expect(element).toHaveStyle('color: #FFF');
-test('theme button should toggle styles', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+test('theme button should toggle styles', async() => {
+  const { getByText } = render(<App />);
+  const themeButtonToggle = getByText('Current theme: light');
+  const containerElement = getByText('Welcome, party people!').closest('div');
+  expect(containerElement).toHaveStyle('background-color: rgb(255, 255, 255)');
+  fireEvent.click(themeButtonToggle);
+  await waitFor(()=>expect(containerElement).toHaveStyle('background-color: #333'));
+  await waitFor(()=>expect(containerElement).toHaveStyle('color: rgb(255, 255, 255)'));
+  fireEvent.click(themeButtonToggle);
+  await waitFor(()=>expect(containerElement).toHaveStyle('background-color: rgb(255, 255, 255)'));
+  await waitFor(()=>expect(containerElement).toHaveStyle('color: #333'));
 });
 
 /**
@@ -45,7 +56,7 @@ test('theme button should toggle styles', () => {
  * (getByText will throw an error if it is not rendered)
  */
 test('hidden button should toggle hidden content', () => {
-  // TODO: change the expect to actually test something 😉
+  
   expect('no test written').toBe('tested');
 });
 
